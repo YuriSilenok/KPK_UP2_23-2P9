5a76621bd8079529711cea6feffe17598f185583
Andrey-hehe <andreygusev263@gmail.com>
2026-03-31 15:47:09 +0300

    Add files via upload

---
+++ S6/models.py
@@ -0,0 +1,18 @@
+from peewee import *
+
+db = SqliteDatabase('S6.db')
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+class Specialization(BaseModel):
+    code = CharField(primary_key=True)
+    name = CharField()
+    duration_training = IntegerField()
+
+def createTable():
+    db.create_tables(Specialization)
+
+if __name__ == '__main__':
+    createTable()
