0b47645eb1dddefbc18a6980819669e64f33fc6b
Alek179 <alekseybolotov19@gmail.com>
2026-05-04 14:18:42 +0300

    Update models.py

--- S1/models.py
+++ S1/models.py
@@ -2,7 +2,6 @@ from peewee import *
 
 db = SqliteDatabase('auth.db')
 
-
 class User(Model):
     user_id = IntegerField(primary_key=True)
     login = CharField(unique=True, max_length=50)
@@ -12,12 +11,10 @@ class User(Model):
     class Meta:
         database = db
 
-
 def init_db():
     db.connect()
     db.create_tables([User], safe=True)
     db.close()
-
 
 if __name__ == '__main__':
     init_db()
