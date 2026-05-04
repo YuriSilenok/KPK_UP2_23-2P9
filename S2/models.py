7df4ae5bb496bf8d30934d29ba7c40a51d93e260
Daniil-Mah <daniilmahin17@gmail.com>
2026-05-04 15:22:08 +0300

    Update NotificationSettings models.py

--- S2/models.py
+++ S2/models.py
@@ -6,11 +6,17 @@ class BaseModel(Model):
     class Meta:
         database = db
 
-class User_Profil(BaseModel):
-  first_name = CharField()
-  second_name = CharField()
-  last_name = CharField()
-  telephone = IntegerField()
-  e-mail = CharField()
-  photo = CharField()
-  profil_act = BooleanField(default=True)
+class Profil(BaseModel):
+    first_name = CharField()
+    second_name = CharField()
+    last_name = CharField()
+    telephone = IntegerField()
+    e-mail = CharField()
+    photo = CharField()
+    profil_act = BooleanField(default=True)
+
+class NotificationSettings(BaseModel):
+    notification_turn = BooleanField(default=True)
+
+if __name__ == '__main__':
+    db.create_tables([Profil, NotificationSettings])
