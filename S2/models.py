b9b340ab957e9f207219ef047e96b118dd788361
Daniil-Mah <daniilmahin17@gmail.com>
2026-05-04 15:07:32 +0300

    Update class profil models.py

--- S2/models.py
+++ S2/models.py
@@ -1,1 +1,16 @@
+from peewee import *
 
+db = SqliteDatabase('S2_Profil_datebase.db')
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+class User_Profil(BaseModel):
+  first_name = CharField()
+  second_name = CharField()
+  last_name = CharField()
+  telephone = IntegerField()
+  e-mail = CharField()
+  photo = CharField()
+  profil_act = BooleanField(default=True)
