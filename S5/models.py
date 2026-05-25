4aad9a2072b44875a965b09567944829107be356
kemple5 <vladbelka659@gmail.com>
2026-05-25 14:30:09 +0300

    Add files via upload

--- S5/models.py
+++ S5/models.py
@@ -1,27 +1,34 @@
-from peewee import (SqliteDatabase, Model, CharField, IntegerField, 
-                    ForeignKeyField, BooleanField, Check)
-from typing import Optional
+from peewee import (
+    SqliteDatabase, Model, CharField, IntegerField,
+    ForeignKeyField, BooleanField, Check
+)
 
 db = SqliteDatabase('faculty_service.db')
+
 
 class BaseModel(Model):
     class Meta:
         database = db
 
+
 class Head(BaseModel):
     first_name = CharField(max_length=50)
     last_name = CharField(max_length=50)
 
+
 class Department(BaseModel):
+    id = IntegerField(primary_key=True)
     name = CharField(max_length=150, unique=True)
     abbreviation = CharField(max_length=20, unique=True)
     room_number = IntegerField(constraints=[Check('room_number > 0')])
-    head = ForeignKeyField(Head, backref='departments', on_delete='RESTRICT')
+    head_id = ForeignKeyField(Head, backref='departments', on_delete='RESTRICT', field='id')
     is_active = BooleanField(default=True)
+
 
 def init_db():
     db.connect()
     db.create_tables([Head, Department])
 
+
 if __name__ == '__main__':
-    init_db()
+    init_db() 
