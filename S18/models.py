63b709972b2b2da3149a33b916ab24eb6192cab5
doge3310 <65708897+doge3310@users.noreply.github.com>
2026-06-01 11:28:51 +0300

    update models

--- S18/models.py
+++ S18/models.py
@@ -1,32 +1,42 @@
-from peewee import SqliteDatabase, Model, AutoField, CharField, IntegerField, BooleanField, ForeignKeyField, Check
+from peewee import (
+    SqliteDatabase, Model, AutoField, CharField,
+    IntegerField, BooleanField, ForeignKeyField, Check
+)
 
 db = SqliteDatabase("database.db")
 
 
-class Table(Model):
+class BaseModel(Model):
     class Meta:
         database = db
 
 
-class Equipment(Table):
+class Equipment(BaseModel):
     id = AutoField()
     name = CharField()
+    is_active = BooleanField(default=True)
+
+    def delete(self):
+        self.is_active = False
+        self.save()
 
 
-class RoomEquipment(Table):
-    room_id = IntegerField(constraints=[Check('room_id > 0')])
-    equipment_id = ForeignKeyField(Equipment, constraints=[Check('equipment_id > 0')])
+class RoomEquipment(BaseModel):
+    room = IntegerField(constraints=[Check('room_id > 0')])
+    equipment = ForeignKeyField(Equipment, backref='room_equipment')
     is_active = BooleanField(default=True)
 
     class Meta:
-        primary_key = False
         indexes = (
-            (('room_id', 'equipment_id'), True),
+            (('room', 'equipment'), True),
         )
+
+    def delete(self):
+        self.is_active = False
+        self.save()
 
 
 if __name__ == "__main__":
     db.create_tables([
         Equipment,
-        RoomEquipment
-    ])
+        RoomEquipment])
