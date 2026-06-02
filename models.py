4d9696162b31e2eb8d73dabc60cef7975a8b75d5
M2K-5F <ivan.semjonow15@gmail.com>
2026-06-02 09:39:33 +0300

    база

---
+++ models.py
@@ -0,0 +1,34 @@
+from peewee import *
+from datetime import datetime
+
+db = SQLiteDatabase('employee_service.db')
+
+class Table(Model):
+    id = AutoField()
+    is_deleted = BooleanField(default=False)
+
+
+    def mark_as_delete(self):
+        self.is_deleted = True
+
+
+    class Meta:
+        database = db
+
+
+class JobPosition(Table):
+    name = CharField(max_length=255, null=False, unique=True)
+    rate = IntegerField(null=False)
+    is_part_time = BooleanField(null=False, default=False)
+
+
+class EmployeePosition(Table):
+    employee_id = ForeignKeyField('Employee')
+    position_id = ForeignKeyField(JobPosition)
+    assigned_at = DateTimeField(null=False, default=datetime.now)
+
+
+class Absence(Table):
+    employee_id = ForeignKeyField('Employee')
+    start_time = DateTimeField(null=False)
+    end_time = DateTimeField(null=False)
