22d24eaa901a6c643e4c21d394bdeb1275e4e0bb
Daniil-Mah <daniilmahin17@gmail.com>
2026-06-04 14:46:04 +0300

    Update models.py

--- S2/models.py
+++ S2/models.py
@@ -9,18 +9,18 @@ class BaseModel(Model):
 
 class Profile(BaseModel):
     id = PrimaryKeyField()
-    full_name = CharField()
+    full_name = CharField(max_length=255)
     telephone = CharField(min_length=10, max_length=10, unique=True)
     email = CharField(max_length=254, unique=True)
-    path_to_photo = CharField()
+    path_to_photo = CharField(max_length=255)
     is_active = BooleanField(default=True)
 
 
 class NotificationSettings(BaseModel):
     id = PrimaryKeyField()
     profile_id = ForeignKeyField(Profile, backref='notification_settings')
-    parameter = CharField()
-    value = CharField()
+    parameter = CharField(max_length=255)
+    value = CharField(max_length=255)
 
 def init_db():
     db.create_tables([Profile, NotificationSettings])
